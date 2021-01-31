639bd6f68605ba82d6864ad91010899449610e16
Denis Alekseenko <den.effective@gmail.com>
2021-01-31 15:11:41 +0200

    unit test FinishOrders

--- src/components/__tests__/FinishOrders.test.tsx
+++ src/components/__tests__/FinishOrders.test.tsx
@@ -1,6 +1,9 @@
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import { FinishOrders } from '../FinishOrders';
+import { IFinishOrder } from 'interfaces/Orders';
+import Col from 'react-bootstrap/Col';
 
 const props = {
   user: {
@@ -8,7 +11,20 @@ const props = {
   },
 };
 
+const setUp = (props: IFinishOrder) => shallow(<FinishOrders {...props} />);
+
 describe('FinishOrders component', () => {
+  let component: any;
+
+  beforeEach(() => {
+    component = setUp(props);
+  });
+
+  it('should be defined', () => {
+    const wrapper = component.find(Col);
+    expect(wrapper).toBeDefined();
+  });
+
   it('should match snapshot', () => {
     const wrapper = renderer.create(<FinishOrders {...props} />).toJSON();
     expect(wrapper).toMatchSnapshot();
