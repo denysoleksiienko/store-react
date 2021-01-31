26f59b79043475e0e832273fccc687ad5b92c67d
Denis Alekseenko <den.effective@gmail.com>
2021-01-31 15:06:05 +0200

    unit test SelectCountry

--- src/components/__tests__/CountrySelect.test.tsx
+++ src/components/__tests__/CountrySelect.test.tsx
@@ -2,6 +2,8 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import { Typeahead } from 'react-bootstrap-typeahead';
 
+import { CountrySelect } from '../CountrySelect';
+
 const props = {
   id: 'basic-typeahead-single',
   placeholder: 'Select Country',
@@ -12,14 +14,19 @@ const props = {
   isInvalid: false,
 };
 
-describe('Button component', () => {
-  it('should be defined', () => {
+describe('CountrySelect component', () => {
+  it('should be defined CountrySelect', () => {
+    const wrapper = shallow(<CountrySelect {...props} />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should be defined Typeahead', () => {
     const wrapper = shallow(<Typeahead {...props} />);
     expect(wrapper).toBeDefined();
   });
 
   it('should match snapshot', () => {
-    const wrapper = renderer.create(<Typeahead {...props} />).toJSON();
+    const wrapper = renderer.create(<CountrySelect {...props} />).toJSON();
     expect(wrapper).toMatchSnapshot();
   });
 });
