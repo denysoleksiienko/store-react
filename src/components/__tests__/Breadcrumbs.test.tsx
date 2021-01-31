e2ac4952658452b3b13228007083d205b05b3b65
Denis Alekseenko <den.effective@gmail.com>
2021-01-31 14:54:30 +0200

    unit test Breadcrumbs

--- src/components/__tests__/Breadcrumbs.test.tsx
+++ src/components/__tests__/Breadcrumbs.test.tsx
@@ -1,17 +1,45 @@
+import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { shallow, ShallowWrapper } from 'enzyme';
 
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import { Breadcrumbs } from '../Breadcrumbs';
-import { BreadcrumbWrap, NavLinkStyled } from 'styled/Breadcrumbs';
+import { BreadcrumbWrap } from 'styled/Breadcrumbs';
+
+const setUp = () => shallow(<Breadcrumbs />);
 
 describe('Breadcrumbs component', () => {
+  let component: any[] | ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+
+  beforeEach(() => {
+    component = setUp();
+  });
+
   it('should be defined', () => {
     const wrapper = shallow(<Breadcrumbs />);
     expect(wrapper).toBeDefined();
   });
 
+  it('should contain wrapper with styled component', () => {
+    const wrapper = component.find(BreadcrumbWrap);
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('should contain three links', () => {
+    const links = component.find(Breadcrumb.Item);
+
+    expect(links.length).toBe(3);
+  });
+
   it('should match snapshot', () => {
-    const wrapper = renderer.create(<Breadcrumbs />).toJSON();
+    const wrapper = renderer
+      .create(
+        <Router>
+          <Breadcrumbs />
+        </Router>
+      )
+      .toJSON();
     expect(wrapper).toMatchSnapshot();
   });
 });
