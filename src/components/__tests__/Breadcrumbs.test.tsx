ac5a3f7ffe4aac592960187d4e3f67e4dcf21615
Denis Alekseenko <den.effective@gmail.com>
2021-01-31 14:45:41 +0200

    lf

--- src/components/__tests__/Breadcrumbs.test.tsx
+++ src/components/__tests__/Breadcrumbs.test.tsx
@@ -1,9 +1,15 @@
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import { Breadcrumbs } from '../Breadcrumbs';
 import { BreadcrumbWrap, NavLinkStyled } from 'styled/Breadcrumbs';
 
 describe('Breadcrumbs component', () => {
+  it('should be defined', () => {
+    const wrapper = shallow(<Breadcrumbs />);
+    expect(wrapper).toBeDefined();
+  });
+
   it('should match snapshot', () => {
     const wrapper = renderer.create(<Breadcrumbs />).toJSON();
     expect(wrapper).toMatchSnapshot();
